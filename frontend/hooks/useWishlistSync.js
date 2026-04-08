9095c914ba1ea04262e2ff12f4860bc7ede2fff2
Nam Nguyen <nhnam23304@gmail.com>
2026-04-09 02:01:17 +0700

    code tiep

---
+++ frontend/hooks/useWishlistSync.js
@@ -0,0 +1,16 @@
+
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setWishlist, selectWishlistItems } from '../redux/wishlistSlice';
+
+const useWishlistSync = () => {
+    const dispatch = useDispatch();
+    const wishlistItems = useSelector(selectWishlistItems);
+
+    useEffect(() => {
+        localStorage.setItem('wishlist_redux', JSON.stringify(wishlistItems));
+    }, [wishlistItems]);
+};
+
+export default useWishlistSync;
