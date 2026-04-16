da54a9c48a77fd1a9c3278478494ea38599b3f2c
Tấn Toàn <22130294@st.hcmuaf.edu.vn>
2026-04-16 15:49:18 +0700

    them route profile vao app

---
+++ frontend/app/(main)/profile/page.tsx
@@ -0,0 +1,10 @@
+import MainLayout from '@/components/MainLayout';
+import UserProfile from '@/components/Profile/UserProfile';
+
+export default function ProfilePage() {
+    return (
+        <MainLayout>
+            <UserProfile />
+        </MainLayout>
+    );
+}
