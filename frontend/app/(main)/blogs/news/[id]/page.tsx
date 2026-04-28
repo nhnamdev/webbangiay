6f6e846cdc1be7f44c314cd1da225dc6e4dcfb95
Tấn Toàn <22130294@st.hcmuaf.edu.vn>
2026-04-28 12:53:41 +0700

    route cho tin tuc va chi tiet

---
+++ frontend/app/(main)/blogs/news/[id]/page.tsx
@@ -0,0 +1,10 @@
+import MainLayout from '@/components/MainLayout';
+import ArticleDetail from '@/components/Blog/ArticleDetail';
+
+export default function ArticleDetailPage() {
+    return (
+        <MainLayout>
+            <ArticleDetail />
+        </MainLayout>
+    );
+}
