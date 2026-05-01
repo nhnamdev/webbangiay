07ff251d4b4adb6a3057b3386d1577702949310e
Tấn Toàn <22130294@st.hcmuaf.edu.vn>
2026-05-01 16:19:29 +0700

    route stores tim cua hang

---
+++ frontend/app/(main)/stores/page.tsx
@@ -0,0 +1,10 @@
+import MainLayout from '@/components/MainLayout';
+import StoreLocator from '@/components/StoreLocator/StoreLocator';
+
+export default function StoresPage() {
+    return (
+        <MainLayout>
+            <StoreLocator />
+        </MainLayout>
+    );
+}
